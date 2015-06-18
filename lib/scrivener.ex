b83c39bd957c75fe30502e76f18f89c143267763
Eike Send <eike.send@gmail.com>
2015-06-18 21:02:44 +0200

    use database asterisk wildcard to count instead of the id column

--- lib/scrivener.ex
+++ lib/scrivener.ex
@@ -150,7 +150,7 @@ defmodule Scrivener do
     |> exclude(:order_by)
     |> exclude(:preload)
     |> exclude(:select)
-    |> select([e], count(e.id))
+    |> select([e], count('*'))
     |> repo.one
   end
 
