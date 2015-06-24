c4fc40d760095f4663d56b41b7c9f82acac2320c
drew <drew@drewolson.org>
2015-06-23 22:11:39 -0500

    Start 0.9.0 development.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Scrivener.Mixfile do
   def project do
     [
       app: :scrivener,
-      version: "0.8.0",
+      version: "0.9.0-dev",
       elixir: "~> 1.0",
       elixirc_paths: elixirc_paths(Mix.env),
       package: package,
