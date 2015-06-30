715293fd906ee34fdb618667579b05d56f6f7c6e
drew <drew@drewolson.org>
2015-06-30 07:43:13 -0500

    Update ecto to 0.13.0.

--- config/test.exs
+++ config/test.exs
@@ -2,6 +2,7 @@ use Mix.Config
 
 config :scrivener, Scrivener.Repo,
   adapter: Ecto.Adapters.Postgres,
+  pool: Ecto.Adapters.SQL.Sandbox,
   database: "scrivener_test",
   username: System.get_env("SCRIVENER_DB_USER") || System.get_env("USER")
 
