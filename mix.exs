3b302aaeda55a07df99e5702250fe883253c3193
drew <drew@drewolson.org>
2016-02-13 11:34:15 -0600

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Scrivener.Mixfile do
   def project do
     [
       app: :scrivener,
-      version: "1.1.1",
+      version: "1.1.2",
       elixir: "~> 1.0",
       elixirc_paths: elixirc_paths(Mix.env),
       package: package,
