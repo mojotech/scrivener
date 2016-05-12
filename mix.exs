66bb1bd5da7ad95e03e7a5abdd139026c85cec0c
drew <drew@drewolson.org>
2016-05-12 07:39:07 -0500

    Move to only supporting elixir 1.2+

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Scrivener.Mixfile do
     [
       app: :scrivener,
       version: "2.0.0-dev",
-      elixir: "~> 1.0",
+      elixir: "~> 1.2",
       package: package,
       description: "Pagination for the Elixir ecosystem",
       deps: deps,
