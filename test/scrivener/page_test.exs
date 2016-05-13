89d80fedb84ddb32197c140cb7351f3e0518694b
drew <drew@drewolson.org>
2016-05-13 15:18:45 -0500

    Fix failing page test

--- test/scrivener/page_test.exs
+++ test/scrivener/page_test.exs
@@ -1,16 +1,15 @@
 defmodule Scrivener.PageTest do
   use Scrivener.TestCase
 
-  alias Scrivener.Post
   alias Scrivener.Page
 
   describe "enumerable" do
     it "implements enumerable" do
-      post1 = %Post{title: "post 1"}
-      post2 = %Post{title: "post 2"}
+      post1 = %{title: "post 1"}
+      post2 = %{title: "post 2"}
       page = %Page{entries: [post1, post2]}
 
-      titles = Enum.map(page, fn entry -> entry.title end)
+      titles = Enum.map(page, &Map.get(&1, :title))
 
       assert titles == ["post 1", "post 2"]
     end
