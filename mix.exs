af9199fa0d97b7f0f417225ac37b3bd43bf87730
drew <drew@drewolson.org>
2016-05-19 08:00:08 -0500

    Move from dialyzer to dialyxir

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule Scrivener.Mixfile do
 
   defp deps do
     [
-      {:dialyze, "~> 0.2.0", only: :dev},
+      {:dialyxir, "~> 0.3.0", only: :dev},
       {:earmark, ">= 0.0.0", only: :dev},
       {:ex_doc, "~> 0.11.0", only: :dev},
       {:ex_spec, "~> 1.0", only: :test}
