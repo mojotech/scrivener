2bdcf2a700e4ea3ac24d95614161bfdae8161b00
drew <drew@drewolson.org>
2016-08-20 12:12:31 -0400

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -26,9 +26,8 @@ defmodule Scrivener.Mixfile do
 
   defp deps do
     [
-      {:dialyxir, "~> 0.3.0", only: :dev},
       {:earmark, ">= 0.0.0", only: :dev},
-      {:ex_doc, "~> 0.11.0", only: :dev},
+      {:ex_doc, "~> 0.13.0", only: :dev},
     ]
   end
 
