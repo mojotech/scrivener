2de6f1d2233997b113b691335ac309cd5f48ce80
drew <drew@drewolson.org>
2017-01-05 07:13:04 -0600

    Update travis yml

--- mix.exs
+++ mix.exs
@@ -6,9 +6,9 @@ defmodule Scrivener.Mixfile do
       app: :scrivener,
       version: "2.3.0-dev",
       elixir: "~> 1.2",
-      package: package,
+      package: package(),
       description: "Pagination for the Elixir ecosystem",
-      deps: deps,
+      deps: deps(),
       docs: [
         main: "readme",
         extras: [
