725cd09e9c8757706949f8bda10d5a536b540b9a
drew <drew@drewolson.org>
2019-02-08 12:23:09 -0600

    Bump version to 2.6.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Scrivener.Mixfile do
   def project do
     [
       app: :scrivener,
-      version: "2.6.0-dev",
+      version: "2.6.0",
       elixir: "~> 1.2",
       package: package(),
       description: "Pagination for the Elixir ecosystem",
